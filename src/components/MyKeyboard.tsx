f9b6f15e6c4636a8ba63b555da602d37b48a99c1
Juan Vazquez <vazquez.juan.1fm@gmail.com>
2022-10-17 11:59:39 -0500

    Calculator Styles

---
+++ src/components/MyKeyboard.tsx
@@ -0,0 +1,20 @@
+import * as React from 'react';
+import Button  from './Button';
+import { View, Text } from 'react-native';
+import { Styles } from '../styles/GlobalStyles';
+import { myColors } from '../styles/Colors';
+
+export default function MyKeyboard() {
+    const [firstNumber, setFirstNumber] = React.useState("");
+    const [secondNumber, setSecondNumber] = React.useState("");
+    const [operation, setOperation] = React.useState("");
+    const [result, setResult] = React.useState<Number | null >(null);
+
+    const  handleNumberPress
+    return (
+        <View>
+
+        </View>
+
+    );
+}
