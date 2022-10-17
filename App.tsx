c0a94018794f2775dee70e757582ecb9db775b32
Juan Vazquez <vazquez.juan.1fm@gmail.com>
2022-10-17 13:39:09 -0500

    Calculadora funciones basicas

--- App.tsx
+++ App.tsx
@@ -1,21 +1,23 @@
 import {useState} from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Switch } from 'react-native';
+import { StyleSheet, Text, View, Switch, SafeAreaView } from 'react-native';
 import { ThemeContext } from './src/context/ThemeContext';
 
 import { myColors } from './src/styles/Colors';
 import Button from './src/components/Button';
+import MyKeyboard from './src/components/MyKeyboard';
 
 export default function App() {
   const [theme, setTheme] = useState('light')
   return (
     <ThemeContext.Provider value={theme}>
-          <View style={ theme === 'light' ? styles.container : [styles.container, { backgroundColor:'#000'}]}>
+          <SafeAreaView style={ theme === 'light' ? styles.container : [styles.container, { backgroundColor:'#000'}]}>
             <Switch
               value={theme === 'light'}
               onValueChange={() => setTheme(theme === 'light' ? 'dark' : 'light')}
             />
-          </View>
+            <MyKeyboard />
+          </SafeAreaView>
 
     </ThemeContext.Provider>
 
@@ -27,6 +29,7 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: myColors.light,
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'flex-start',
+    paddingTop: 25
   },
 });
